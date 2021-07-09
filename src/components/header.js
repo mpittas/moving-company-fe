425324e53bbf29c1d86ad28a371cc91d4d5f1c1f
mpittas <webpittas@gmail.com>
2021-07-09 20:50:37 +0300

    Add colors and style header for desktop

--- src/components/header.js
+++ src/components/header.js
@@ -3,31 +3,60 @@ import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import "@fontsource/poppins";
 
+import logo from '../images/logo.svg';
+
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `#ccc`,
-      marginBottom: `20px`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `#000`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <header className="mc-header">
+    <div className="mc-header__left">
+      <div className="mc-header__logo">
+        <img src={logo} alt="Header logo"/>
+      </div>
+
+      <div className="mc-header__nav">
+        <nav>
+          <ul>
+            <li className="active">
+              <a href="#">
+                Moving
+              </a>
+            </li>
+
+            <li>
+              <a href="#">
+                Services
+              </a>
+            </li>
+
+            <li>
+              <a href="#">
+                Eco Mission
+              </a>
+            </li>
+
+            <li>
+              <a href="#">
+                Moving Tips
+              </a>
+            </li>
+
+            <li>
+              <a href="#">
+                Reviews
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+
+    <div className="mc-header__actions">
+      <a href="#" className="mc-btn">
+        Book an Hour
+      </a>
+
+      <a href="#" className="mc-btn mc-btn-dark">
+        Pricing Plans
+      </a>
     </div>
   </header>
 )
