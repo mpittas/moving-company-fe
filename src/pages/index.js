ed269209c873af2e20e2f364df878353160d1e0d
mpittas <webpittas@gmail.com>
2021-07-10 22:00:23 +0300

    Work on hero section

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,18 +4,29 @@ import * as React from "react"
 import Layout from "../components/layout"
 import '../styles/global.scss'
 
+
+import heroIcon from '../images/moving-truck.svg';
+import heroImage from '../images/hero-image.jpg';
+
 // import Seo from "../components/seo"  
 
 const IndexPage = () => (
   <Layout>
     <div className="hero">
       <div className="container">
-        <div className="hero-left">
-          left
-        </div>
-        
-        <div className="hero-left">
-          right
+        <div className="hero__inner">
+          <div className="hero__left">
+            <img src={heroIcon} alt="Moving truck in motion"/>
+            <h2>Move your home effortlessly with our help</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <div>
+              <a href="#" className="mc-btn">Book your Hour</a>
+            </div>
+          </div>
+          
+          <div className="hero__right">
+            <img src={heroImage} alt="Group photo"/>
+          </div>
         </div>
       </div>
     </div>
