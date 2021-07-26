6c2f8bedf7f6e356d1e1d278093e65ba739b6197
mpittas <webpittas@gmail.com>
2021-07-26 17:10:37 +0300

    Import bootstrap styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,13 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
+import "@fontsource/poppins";
 import "./layout.css"
-import "@fontsource/poppins"; // Defaults to weight 400.
+
+// Import main bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
